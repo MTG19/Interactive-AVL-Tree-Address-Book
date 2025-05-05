c8393d822cf66c11bd0874ffc60388606c0c4e96
Menna <manameno2005yahoo.com@gmail.com>
2025-05-06 00:56:29 +0300

    files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/LENOVO/CLionProjects/Data structure/Interactive-AVL-Tree-Address-Book
+# Install script for directory: C:/Users/LENOVO/CLionProjects/Data structure/AVL-Tree-Address-Book
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/LENOVO/CLionProjects/Data structure/Interactive-AVL-Tree-Address-Book/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/LENOVO/CLionProjects/Data structure/AVL-Tree-Address-Book/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
