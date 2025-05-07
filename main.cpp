dc94e7a584e958e85a549ca4d0b2df47317851ec
Menna <manameno2005yahoo.com@gmail.com>
2025-05-07 21:09:23 +0300

    functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+struct Contact {
+    string name;
+    string phone;
+    string email;
+};
+
+struct Node {
+    int id; // key
+    Contact contact; // data
+    Node* left;
+    Node* right;
+    int height;
+
+    Node(int id, Contact contact) {
+        this->id = id;
+        this->contact = contact;
+        left = right = nullptr;
+        height = 1;
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Welcome to the Contact Management System!" << endl;
     return 0;
 }
